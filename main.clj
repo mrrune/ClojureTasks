773b556e009f0256bae71108e182c1f9767231be
mrrune <mrrune@me.com>
2019-10-23 10:51:06 +0300

    Task Lazy-seq

--- main.clj
+++ main.clj
@@ -25,3 +25,11 @@
     )
   )
 )
+
+
+;; Task 62
+;; Given a side-effect free function f and an initial value x 
+;; write a function which returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc.
+(fn fun [userFun x]
+  (lazy-seq (cons x (fun userFun (userFun x) )))
+)
