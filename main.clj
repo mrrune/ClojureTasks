8ff2672437c3fd61664ca8cad02f4b3526ba690b
mrrune <mrrune@me.com>
2019-12-02 10:08:39 +0300

    Task 110

--- main.clj
+++ main.clj
@@ -183,3 +183,26 @@
       )
     )
 )
+
+;; Task 110
+;; Write a function that returns a lazy sequence of "pronunciations" of a sequence of numbers.
+;; A pronunciation of each element in the sequence consists of the number of repeating identical numbers 
+;; and the number itself. For example, [1 1] is pronounced as [2 1] ("two ones"), 
+;; which in turn is pronounced as [1 2 1 1] ("one two, one one").
+
+;; Your function should accept an initial sequence of numbers, and return an infinite lazy sequence of pronunciations, 
+;; each element being a pronunciation of the previous element.
+
+(fn fun [col]
+  (let [x (reduce
+            #(conj % (count %2) (first %2))
+            []
+            (partition-by identity col)
+          )
+        ]
+    (cons
+      x
+      (lazy-seq (fun x))
+    )
+  )
+)
