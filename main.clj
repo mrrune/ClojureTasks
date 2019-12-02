345321716a0012de98534d8b9212e37ee97f3a33
mrrune <mrrune@me.com>
2019-12-02 11:32:31 +0300

    Task 108

--- main.clj
+++ main.clj
@@ -206,3 +206,30 @@
     )
   )
 )
+
+;; Task 108
+;; Given any number of sequences, each sorted from smallest to largest, find the smallest single number
+;; which appears in all of the sequences. The sequences may be infinite, so be careful to search lazily.
+;; (= 7 (__ (range) (range 0 100 7/6) [2 3 5 7 11 13]))
+;;(= 64 (__ (map #(* % % %) (range)) ;; perfect cubes
+;;        (filter #(zero? (bit-and % (dec %))) (range)) ;; powers of 2
+;;         (iterate inc 20))) ;; at least as large as 20
+(fn [& args]
+  (loop [counters (take (count args) (repeat 0))]
+      (let [elements (map
+                    #(first (drop % %2))
+                    counters
+                    args
+                  )
+          maxElem (apply max elements)
+          minElem (apply min elements)
+      ]
+
+        (if (= minElem maxElem) 
+          minElem
+          (recur (map
+                    #(if (not= % maxElem) (inc %2) %2 )
+                    elements
+                    counters
+                  )
+          )))))
